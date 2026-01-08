22b53c8388e231caced27fa721063d959364f906
Aryan Chaudhary <j.aryanchaudhary@gmail.com>
2026-01-08 22:17:28 +0530

    Fix folder structure and serve frontend correctly

--- backend/server.js
+++ backend/server.js
@@ -31,10 +31,9 @@ app.use("/api/users", userRoutes);
 app.use(express.static(path.join(__dirname, "dist")));
 
 app.get("*", (req, res) => {
-  res.sendFile(
-    path.join(__dirname, "dist", "index.html")
-  );
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
 });
+
 
 server.listen(PORT, () => {
   connectToMongoDB();
